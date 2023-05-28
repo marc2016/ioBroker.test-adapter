a193e0576715904ad62c7c4f5d010c0bfe0d8ef8
Marc Lammers <marc@lammers.dev>
2023-05-28 07:24:57 +0200

    First commit

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+    'test-adapter adapter settings': {
+        en: 'Adapter settings for test-adapter',
+        de: 'Adaptereinstellungen für test-adapter',
+        ru: 'Настройки адаптера для test-adapter',
+        pt: 'Configurações do adaptador para test-adapter',
+        nl: 'Adapterinstellingen voor test-adapter',
+        fr: "Paramètres d'adaptateur pour test-adapter",
+        it: "Impostazioni dell'adattatore per test-adapter",
+        es: 'Ajustes del adaptador para test-adapter',
+        pl: 'Ustawienia adaptera dla test-adapter',
+        uk: 'Налаштування адаптера для test-adapter',
+        'zh-cn': 'test-adapter的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        uk: 'варіант1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'вариант 2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        uk: 'варіант2',
+        'zh-cn': '选项2',
+    },
+};
